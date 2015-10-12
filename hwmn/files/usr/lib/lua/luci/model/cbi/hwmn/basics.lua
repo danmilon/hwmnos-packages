cfea4a4b06ce40fa105432970a3413d3c39f824a
Dan Milon <i@danmilon.me>
2015-10-13 02:32:32 +0300

    add support for new wireless links

--- hwmn/files/usr/lib/lua/luci/model/cbi/hwmn/basics.lua
+++ hwmn/files/usr/lib/lua/luci/model/cbi/hwmn/basics.lua
@@ -16,21 +16,30 @@ local ip = require "luci.ip"
 local sys = require "luci.sys"
 
 hwmn = Map("hwmn", "HWMN", translate("HWMN specific configuration"))
-hwmn_generic = hwmn:section(TypedSection, "hwmn", "generic")
+hwmn_generic = hwmn:section(TypedSection, "generic", "Generic")
 hwmn_generic.anonymous = true
 enabled = hwmn_generic:option(
    Flag,
    "enabled",
    translate("Enable easy HWMN set up. If not checked, no further changes will be made to your device."))
 
-hwmn_network = hwmn:section(TypedSection, "hwmn", "network")
-hwmn_network.anonymous = true
-ipv4_range = hwmn_network:option(
-  Value,
-  "ipv4_range",
-  translate("The IPv4 range given to you in the form of 10.176.XXX.XXX/XX"))
+node_name = hwmn_generic:option(
+   Value,
+   "node_name",
+   translate("The name of this HWMN node."))
+node_name.datatype = "hostname"
+function node_name.write(self, section, value)
+   Value.write(self, section, value)
+   sys.hostname("router." .. value .. ".node.her.wn")
+end
 
-function ipv4_range.validate(self, value)
+hwmn_network = hwmn:section(TypedSection, "network", "Networking")
+hwmn_network.anonymous = true
+ipv4_subnet = hwmn_network:option(
+  Value,
+  "ipv4_subnet",
+  translate("The IPv4 subnet given to you in the form of 10.176.XXX.XXX/XX. When you apply the settings, you will have to reconnect to the network and the router will be available under the IP address \"10.176.XXX.1\"."))
+function ipv4_subnet.validate(self, value)
    if not value then
      return nil
    end
@@ -47,18 +56,22 @@ function ipv4_range.validate(self, value)
    end
 end
 
-system = Map("system", translate("Basic system settings"))
-system.anonymous = true
-b = system:section(TypedSection, "system")
-b.anonymous = true
+hwmn_links = hwmn:section(TypedSection, "link", "Links")
+hwmn_links.anonymous = true
+hwmn_links.addremove = true
+hwmn_links:option(
+   Value,
+   "destination_node_name",
+   translate("The name of the destination HWMN node."))
 
-hostname = b:option(Value, "hostname", translate("Name of this node"))
-hostname.rmempty = false
-hostname.datatype = "hostname"
+hwmn_links:option(
+   Value,
+   "ipv4_subnet",
+   translate("The IPV4 subnet assigned to this link."))
 
-function hostname.write(self, section, value)
-   Value.write(self, section, value)
-   sys.hostname(value)
-end
+hwmn_links:option(
+   Value,
+   "ifname",
+   translate("Name of interface connecting to the antenna."))
 
-return hwmn, system
+return hwmn
